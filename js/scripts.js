8f536f6a67fab1776c61a3eee2cfd4392164eb99
astonevart <astonevart@gmail.com>
2017-04-22 02:56:32 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$('#carousel').elastislide();
+
+
+$(".alex1").click(function(){
+    $(".fedshow").removeClass('active');
+    $("#alex").addClass('active');
+});
+
+$(".olya1").click(function(){
+    $(".fedshow").removeClass('active');
+    $("#olya").addClass('active');
+});
+
+$(".jenya1").click(function(){
+    $(".fedshow").removeClass('active');
+    $("#jenya").addClass('active');
+});
+
+$(".stas1").click(function(){
+    $(".fedshow").removeClass('active');
+    $("#stas").addClass('active');
+});
+$(".dima1").click(function(){
+    $(".fedshow").removeClass('active');
+    $("#dima").addClass('active');
+});
